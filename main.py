4ea9c63fdcea8eb0c7e207e1f697b604042e8cce
Roseztt <rosezhaott@gmail.com>
2025-06-07 23:11:58 -0400

    Finished challege part 1 and 2

--- main.py
+++ main.py
@@ -16,12 +16,40 @@ def main():
     # set the cur time to 8pm
     curr_time = curr_time.replace(hour=20, minute=0, second=0, microsecond=0)
 
+    completed_reminders = set()
+    daily_reminders_published = {}
+
     current_house_data = fake_home_data.get()
     while current_house_data is not None:
         print("current time is", curr_time.strftime("%H:%M:%S"))
         
-        # eval function should look like:
-        # eval_func(curr_time, current_house_data)
+        for reminder in home_config:
+            name = reminder["name"]
+            trigger_type = reminder["trigger_type"]
+            repeats = reminder["repeats"]
+            code = reminder["code"]
+
+            if name in completed_reminders:
+                continue
+
+            if repeats == "daily":
+                if daily_reminders_published.get(name) == curr_time.date():
+                    continue
+
+            funcs = {}
+            exec(code, funcs)
+            eval_func_name = code.split("(")[0].split(" ")[1]
+            eval_func = funcs[eval_func_name]
+
+            if eval_func(curr_time, current_house_data):
+                publish(name)
+
+                if repeats == "daily":
+                    daily_reminders_published[name] = curr_time.date()
+                elif trigger_type == "once" or repeats == "never":
+                    completed_reminders.add(name)
+
+
         
         current_house_data = fake_home_data.get()
         curr_time += timedelta(hours=1)
